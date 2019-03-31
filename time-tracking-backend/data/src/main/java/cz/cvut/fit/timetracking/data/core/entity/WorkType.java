926784422e48f437e6cdfa83a52754fab55b6cd6
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-03-31 19:16:11 +0200

    Add work type hibernate mapping

---
+++ time-tracking-backend/data/src/main/java/cz/cvut/fit/timetracking/data/core/entity/WorkType.java
@@ -0,0 +1,50 @@
+package cz.cvut.fit.timetracking.data.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Table(name = "work_type")
+public class WorkType {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @NotEmpty
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
