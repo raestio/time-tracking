c195c86d6bba0e7e7b94a5285a59e5fee1376e92
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-03-31 16:49:36 +0200

    Add project entity mapping

---
+++ time-tracking-backend/data/src/main/java/cz/cvut/fit/timetracking/data/core/entity/Project.java
@@ -0,0 +1,77 @@
+package cz.cvut.fit.timetracking.data.core.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+
+/**
+ * @author Rastislav Zlacky (rastislav.zlacky@inventi.cz) on 26.03.2019.
+ */
+@Entity
+@Table(name = "project")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @NotNull
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @NotNull
+    @Column(name = "start")
+    private LocalDate start;
+
+    @Column(name = "end")
+    private LocalDate end;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getStart() {
+        return start;
+    }
+
+    public void setStart(LocalDate start) {
+        this.start = start;
+    }
+
+    public LocalDate getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalDate end) {
+        this.end = end;
+    }
+}
