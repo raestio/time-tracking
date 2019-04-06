a038370eeb649d8da794d7fc356bafe394e4b27e
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-04-06 13:41:39 +0200

    Refactor

--- time-tracking-backend/app/src/main/java/cz/cvut/fit/timetracking/app/TimeTrackingApplication.java
+++ time-tracking-backend/app/src/main/java/cz/cvut/fit/timetracking/app/TimeTrackingApplication.java
@@ -1,9 +1,6 @@
 package cz.cvut.fit.timetracking.app;
 
 import cz.cvut.fit.timetracking.configuration.TimeTrackingApplicationConfiguration;
-import cz.cvut.fit.timetracking.data.api.DataAccessApi;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -11,17 +8,9 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @Import(TimeTrackingApplicationConfiguration.class)
-public class TimeTrackingApplication implements CommandLineRunner {
+public class TimeTrackingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TimeTrackingApplication.class, args);
 	}
-
-	@Autowired
-	private DataAccessApi dataAccessApi;
-
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println(dataAccessApi.findUserById(null));
-    }
 }
