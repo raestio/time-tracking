5dc59b62787a9f2da1ee33b156c1fde3c3938335
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-04-07 23:33:40 +0200

    Add more controllers

---
+++ time-tracking-backend/rest/src/main/java/cz/cvut/fit/timetracking/rest/controller/WorkRecordController.java
@@ -0,0 +1,9 @@
+package cz.cvut.fit.timetracking.rest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/work-records")
+public class WorkRecordController {
+}
