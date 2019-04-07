48e8fbcc1b16ee362b325ec53fa5c77f4e20e447
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-04-07 18:53:06 +0200

    Optimize import

--- time-tracking-backend/rest/src/main/java/cz/cvut/fit/timetracking/rest/controller/UserController.java
+++ time-tracking-backend/rest/src/main/java/cz/cvut/fit/timetracking/rest/controller/UserController.java
@@ -10,7 +10,6 @@ import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
