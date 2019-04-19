614e2d9b1cc560b7208beae98f498fc3591b5a4a
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-04-19 21:18:31 +0200

    Fix url

--- google-login-poc-app/src/util/APIUtils.js
+++ google-login-poc-app/src/util/APIUtils.js
@@ -33,11 +33,3 @@ export function getCurrentUser() {
         method: 'GET'
     });
 }
-
-export function login(loginRequest) {
-    return request({
-        url: API_BASE_URL + "/auth/login",
-        method: 'POST',
-        body: JSON.stringify(loginRequest)
-    });
-}
