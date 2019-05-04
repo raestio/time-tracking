bc1a2f1ef1ac0f155811eb331cb73144a0247f46
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-05-04 18:16:57 +0200

    Remove unnecessary  imports

--- time-tracking-backend/report/src/main/java/cz/cvut/fit/timetracking/report/service/impl/ReportServiceImpl.java
+++ time-tracking-backend/report/src/main/java/cz/cvut/fit/timetracking/report/service/impl/ReportServiceImpl.java
@@ -14,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
