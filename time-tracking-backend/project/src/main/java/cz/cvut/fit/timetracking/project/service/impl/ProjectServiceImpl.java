f30fbb95d477bd839e5301ff256e94d5aaf9e787
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-06-01 16:54:56 +0200

    Optimize imports and try to fix jacoco coverage spring services scanning

--- time-tracking-backend/project/src/main/java/cz/cvut/fit/timetracking/project/service/impl/ProjectServiceImpl.java
+++ time-tracking-backend/project/src/main/java/cz/cvut/fit/timetracking/project/service/impl/ProjectServiceImpl.java
@@ -6,7 +6,6 @@ import cz.cvut.fit.timetracking.data.api.dto.ProjectDTO;
 import cz.cvut.fit.timetracking.data.api.dto.ProjectRoleDTO;
 import cz.cvut.fit.timetracking.data.api.dto.WorkTypeDTO;
 import cz.cvut.fit.timetracking.project.dto.Project;
-import cz.cvut.fit.timetracking.project.dto.ProjectAssignment;
 import cz.cvut.fit.timetracking.project.dto.ProjectRole;
 import cz.cvut.fit.timetracking.project.dto.ProjectRoleName;
 import cz.cvut.fit.timetracking.project.dto.WorkType;
@@ -18,7 +17,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
