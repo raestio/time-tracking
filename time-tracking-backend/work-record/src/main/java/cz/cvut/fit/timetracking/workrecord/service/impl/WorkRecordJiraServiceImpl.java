b618478257253693222504512f0fd2ce830e4c2d
Rastislav Zlacky <rasto.zlacky@gmail.com>
2019-06-03 00:44:01 +0200

    Remove code smells - sonarqube

--- time-tracking-backend/work-record/src/main/java/cz/cvut/fit/timetracking/workrecord/service/impl/WorkRecordJiraServiceImpl.java
+++ time-tracking-backend/work-record/src/main/java/cz/cvut/fit/timetracking/workrecord/service/impl/WorkRecordJiraServiceImpl.java
@@ -20,15 +20,12 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.TimeZone;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
